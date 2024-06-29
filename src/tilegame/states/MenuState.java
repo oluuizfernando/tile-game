facb44e4fa2dcec171cc0336b53f2aacd387c966
oluuizfernando <euluizfernando2001@gmail.com>
2024-06-29 15:20:20 -0300

    feat: cria estado de jogo e estado de menu

---
+++ src/tilegame/states/MenuState.java
@@ -0,0 +1,21 @@
+package tilegame.states;
+
+import java.awt.Graphics;
+import tilegame.Game;
+
+public class MenuState extends State {
+
+    public MenuState(Game game) {
+        super(game);
+    }
+
+    @Override
+    public void tick(){
+
+    }
+
+    @Override
+    public void render(Graphics g){
+
+    }
+}
