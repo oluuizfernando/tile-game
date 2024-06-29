9b3b4ff6a962f1f4ad0a2c44bda7e37e4a91c76e
oluuizfernando <euluizfernando2001@gmail.com>
2024-06-28 22:19:08 -0300

    cria assets para carregar somente uma vez as imagens do spritesheet

---
+++ src/tilegame/gfx/Assets.java
@@ -0,0 +1,22 @@
+package tilegame.gfx;
+
+import java.awt.image.BufferedImage;
+
+// usada para carrega romsente 1 vez nossos arquivos
+public class Assets {
+    // qualquer imagem ou musica no jogo
+
+    private static final int width = 32, height = 32;
+
+    public static BufferedImage player, dirt, grass, stone, tree;
+
+    public static void init() {
+        SpriteSheet sheet = new SpriteSheet(ImageLoader.loadImage("/textures/sheet.png"));
+
+        player = sheet.crop(0, 0, width, height);
+        dirt = sheet.crop(width, 0, width, height);
+        grass = sheet.crop(width * 2, 0, width, height);
+        stone = sheet.crop(width * 2, 0, width, height);
+        tree = sheet.crop(0, height * 2, width, height);
+    }
+}
