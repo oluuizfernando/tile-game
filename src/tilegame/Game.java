bb4baf77ff291fcaff5bd2067146f8099c0db996
oluuizfernando <euluizfernando2001@gmail.com>
2024-06-28 23:09:44 -0300

    feat: cria gameloop para rodar o jogo liso baseado em fps

--- src/tilegame/Game.java
+++ src/tilegame/Game.java
@@ -32,8 +32,10 @@ public class Game implements Runnable {
         Assets.init();
     }
 
-    private void tick() {
+    int x = 0;
 
+    private void tick() {
+        x += 1;
     }
 
     private void render(){
@@ -47,7 +49,7 @@ public class Game implements Runnable {
         g = bs.getDrawGraphics();
         g.clearRect(0,0, width, height);
 
-        g.drawImage(Assets.grass, 0, 0, null);
+        g.drawImage(Assets.grass, x, 0, null);
 
         bs.show();
         g.dispose();
@@ -56,9 +58,43 @@ public class Game implements Runnable {
     public void run(){
         init();
 
+        int fps = 60;
+        // maximum amount of time that we are allow
+        // to have to run/tick methods to achieve 60 times per second
+        double timePerTickNanoSeconds = 1000000000 / fps;
+        double delta = 0;
+        long now;
+        long lastTime = System.nanoTime();
+        long timer = 0;
+        int ticks = 0;
+
         while (running) {
-            tick();
-            render();
+            now = System.nanoTime();
+            // basically, now - lastTime we get the amount of time past since we last called this line of code below
+            // that way, we divide that amount of time by the maximum amout of time that we have to run tick/render methods
+            // and we add it to delta variable
+            // this is going to add to delta variable, how much time we have until we call this methos tick and render again
+            // tells the computer when and WHEN NOT to call this tick and render (to see if we need to call them or not)
+            delta += (now - lastTime) / timePerTickNanoSeconds;
+            timer += now - lastTime; //we are going to add the amount of time that past since this block below last ran
+
+            lastTime = now;
+
+            // ou seja, se o delta for maior que 1, tem que tick/render agora
+            // isto é tipo como falar, roda agora porque ja estamos na hora de rodar o proximo frame
+            if (delta >= 1) {
+                tick();
+                render();
+                ticks++;
+                // reseta ele
+                delta--;
+            }
+
+            if (timer >= 1000000000){
+                System.out.println("Frames: " + ticks);
+                ticks = 0;
+                timer = 0;
+            }
         }
 
         stop();
