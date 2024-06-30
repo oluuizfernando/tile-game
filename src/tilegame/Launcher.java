fb532f2fe708940c3a50a1b1b073e8554683f787
oluuizfernando <euluizfernando2001@gmail.com>
2024-06-30 20:45:52 -0300

    feat: cria camera e otimiza criacao de mapas

--- src/tilegame/Launcher.java
+++ src/tilegame/Launcher.java
@@ -1,7 +1,5 @@
 package tilegame;
 
-import tilegame.display.Display;
-
 public class Launcher {
 
     public static void main(String[] args) {
--- src/tilegame/entities/creatures/Creature.java
+++ src/tilegame/entities/creatures/Creature.java
@@ -1,5 +1,6 @@
 package tilegame.entities.creatures;
 
+import tilegame.Game;
 import tilegame.entities.Entity;
 import tilegame.gfx.Assets;
 
@@ -15,8 +16,8 @@ public abstract class Creature extends Entity {
     protected float speed;
     protected float xMove, yMove;
 
-    public Creature(float x, float y, int width, int height) {
-        super(x, y, width, height);
+    public Creature(Game game, float x, float y, int width, int height) {
+        super(game, x, y, width, height);
         health = DEFAULT_HEALTH;
         speed = DEFAULT_SPEED;
         xMove = 0;
